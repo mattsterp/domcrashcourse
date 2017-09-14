a3d543c0706a549ed8b282fcf93f22ae642b2ed6
Matthew Peters <matt@mattsterp.com>
2017-09-14 15:52:20 -0400

    Chore.

--- dom.js
+++ dom.js
@@ -1,6 +1,6 @@
 // EXAMINE THE DOCUMENT OBJECT //
 
-// console.dir(document);
+//  console.dir(document);
 // console.log(document.domain);
 // console.log(document.URL);
 // console.log(document.title);
